76d58074926b6fb42dc64136fba9c48d439449bc
italojames23 <nomesobrenome8080@gmail.com>
2022-02-21 11:16:07 -0300

    add

---
+++ pintando_paredes.py
@@ -0,0 +1,4 @@
+larg = float(input('Digite a largura da parede'))
+alt = float(input('Digite a altura da parede'))
+area = larg*alt / 2
+print(' Para pintar essa parede, será necessario {:.2f}L de tinta'.format(area))
